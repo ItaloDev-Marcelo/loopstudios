fa28df82e29dfb7d8efe6596fc78e8fc0d953afa
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-30 16:11:40 -0300

    style: add mobile style to navItem.tsx

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -2,7 +2,7 @@
 interface NavProps {navLink: string}
 
 const NavItem = ({navLink}:NavProps) => {
-  return <li><a href='#'>{navLink}</a></li>
+  return <li className='text-2xl my-7 font-light  text-gray-200 hover:text-white'><a href='#'>{navLink}</a></li>
 }
 
 export default NavItem
