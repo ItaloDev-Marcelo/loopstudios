fbcb3a0330eb1443afa03930a783b77f1e1e0b8e
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-30 18:11:39 -0300

    refactor: add headerContainer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import  NavBar from './Nav/NavBar'
-
+import HeaderContainer from './header-banner/HeaderContainer'
 function App() {
-   return <NavBar/>
+   return <HeaderContainer><NavBar/></HeaderContainer>
 }
 
 export default App
