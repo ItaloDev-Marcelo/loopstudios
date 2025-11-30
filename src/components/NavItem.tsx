ee44826bb171de378d6dd4b441c1ece4df59c157
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-30 18:08:56 -0300

    style: add more style

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -2,7 +2,8 @@
 interface NavProps {navLink: string}
 
 const NavItem = ({navLink}:NavProps) => {
-  return <li className='text-2xl my-7 font-light  text-gray-200 hover:text-white'><a href='#'>{navLink}</a></li>
+  return <li className='text-2xl  my-7 lg:my-0 lg:text-[1.1em] lg:mx-3.5
+   font-light uppercase lg:capitalize lg:font-semibold  text-gray-200 hover:text-white'><a href='#'>{navLink}</a></li>
 }
 
 export default NavItem
