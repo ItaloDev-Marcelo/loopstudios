eefbe1669f67a2aeb94c68ed63a13cd7dfe460c1
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-01 18:48:59 -0300

    feat: create Image.block.ts

---
+++ src/utils/images.block.ts
@@ -0,0 +1,80 @@
+import MobileCuriosity from '../assets/images/mobile/image-curiosity.jpg';
+import DaskCuriosity from '../assets/images/desktop/image-curiosity.jpg';
+import MobileDeepEath from '../assets/images/mobile/image-deep-earth.jpg';
+import DeskDeepEath from '../assets/images/desktop/image-deep-earth.jpg';
+import Mobilefisheye from '../assets/images/mobile/image-fisheye.jpg';
+import Deskfisheye from '../assets/images/desktop/image-fisheye.jpg';
+import MobileGrid from '../assets/images/mobile/image-grid.jpg';
+import DeskGrid from '../assets/images/desktop/image-grid.jpg';
+import MobileInteractive from '../assets/images/mobile/image-interactive.jpg';
+import DeskInteractive from '../assets/images/desktop/image-interactive.jpg';
+import MobileNight from '../assets/images/mobile/image-night-arcade.jpg';
+import DeskNight from '../assets/images/desktop/image-night-arcade.jpg';
+import MobilePocket from '../assets/images/mobile/image-pocket-borealis.jpg';
+import DeskPocket from '../assets/images/desktop/image-pocket-borealis.jpg';
+import MobileSoccer from '../assets/images/mobile/image-soccer-team.jpg';
+import DeskSoccer from '../assets/images/desktop/image-soccer-team.jpg';
+
+
+export const Images = [
+    MobileCuriosity,
+    DaskCuriosity,
+    MobileDeepEath,
+    DeskDeepEath,
+    Mobilefisheye,
+    Deskfisheye,
+    MobileGrid,
+    DeskGrid,
+    MobileInteractive,
+    DeskInteractive,
+    MobileNight,
+    DeskNight,
+    MobilePocket,
+    DeskPocket,
+    MobileSoccer,
+    DeskSoccer 
+]
+
+
+export const ImagesData = [
+    {
+        id: 0,
+        mobile:Images[0],
+        desk:Images[1]
+    },
+     {
+        id: 1,
+        mobile:Images[2],
+        desk:Images[3]
+    },
+     {
+        id: 2,
+        mobile:Images[4],
+        desk:Images[5]
+    },
+     {
+        id: 3,
+        mobile:Images[6],
+        desk:Images[7]
+    },
+     {
+        id: 4,
+        mobile:Images[8],
+        desk:Images[9]
+    },
+    {
+        id: 5,
+        mobile:Images[10],
+        desk:Images[11]
+    },
+    {
+        id: 6,
+        mobile:Images[12],
+        desk:Images[13]
+    },
+    {
+        id: 7,
+        mobile:Images[14],
+        desk:Images[15]
+    }
+]
