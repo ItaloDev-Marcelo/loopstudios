aa6d0ae6b00167fc04e4a76e02a2bb0af946dbd0
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-15 10:44:44 -0300

    fix: add padding to large devices

--- src/Nav/NavBar.tsx
+++ src/Nav/NavBar.tsx
@@ -8,12 +8,12 @@ export default function NavBar() {
     const [navTab, setNavTab] = useState(false)
     return  (
         <nav className={`${navTab ? 'bg-transparent text-black   lg:px-14 lg:pt-20' :
-         'bg-black h-screen lg:h-auto text-white lg:bg-transparent  '} lg:flex lg:items-center lg:justify-between p-4 lg:p-9 `}>
+         'bg-black h-screen lg:h-auto text-white lg:bg-transparent  '} lg:flex lg:items-center lg:justify-between p-4 lg:p-9 xl:p-11 `}>
              <div className='flex flex-row justify-between items-center mt-2.5 px-2 lg:mt-0'>
                 <a href='index.html' className='font-bold text-2xl text-white'>loopstudios</a>
              <button id='btn' className='flex lg:hidden' onClick={() => setNavTab(!navTab)}>{navTab ? <img src={hamburger} className="is-closed"/>  : <img src={close} className='is-open' />}</button>
              </div>
-             <ul className={`${navTab ? 'hidden lg:flex lg:flex-row lg:items-center px-2' : ' mt-27 lg:mt-0 lg:flex lg:flex-row lg:items-center'}`}>
+             <ul className={`${navTab ? 'hidden lg:flex lg:flex-row lg:items-center px-2 ' : ' mt-27 lg:mt-0 lg:flex lg:flex-row lg:items-center'} xl:px-5`}>
                 {navItems.map((item, index) => (
                     <NavItem navLink={item} key={index} />
                 ))}
