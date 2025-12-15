a0ba45efe585263b0ed2c0ca34d9a657ed471186
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-15 14:06:08 -0300

    feat: create array of images to use on the footer component

---
+++ src/utils/images.block.rodape.ts
@@ -0,0 +1,7 @@
+import Facebook from '../assets/images/icon-facebook.svg';
+import Twitter from '../assets/images/icon-twitter.svg'
+import Pinterest from '../assets/images/icon-pinterest.svg'
+import Instagram from '../assets/images/icon-instagram.svg';
+
+
+export const RodapeImages = [Facebook, Twitter, Pinterest, Instagram]
