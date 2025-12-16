b2c452034d6094e807e2e82ef5b68b4fc4e818aa
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-16 14:20:10 -0300

    style: fix style error on desktop devices

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,7 @@
 import RenderFooter from '../components/RenderFooter'
 const FooterComp = () => {
   return (
-    <footer className='bg-black text-white h-full lg:h-45 lg:mt-10 xl:mt-7 xl:px-15'>
+    <footer className='bg-black text-white h-full lg:h-45 lg:mt-10 xl:mt-7 lg:px-5 xl:px-15'>
         <RenderFooter type={0} />
         <RenderFooter type={1} />
     </footer>
