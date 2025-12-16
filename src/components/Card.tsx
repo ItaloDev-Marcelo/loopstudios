757e4a9d4e13e9c1491f4a5d3e0887574cdb5844
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-16 14:05:31 -0300

    refactor: add interface to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,11 @@
 import RenderImage from '../components/RenderImage';
-
-interface CardProps {
-    mobile: string;
-    desk: string;
-    title: string
-}
-
+import type { CardProps } from '../interface/CardProps';
 const Card = ({mobile, desk, title}:CardProps) => {
   return (
     <div className=' text-white hover:text-black cursor-pointer '>
         <figure className="relative">
             <RenderImage mobile={mobile} desk={desk}  type={0}/>
-            <h3 className="absolute left-6 top-20 md:top-90 xl:top-97 font-medium">{title}</h3>
+            <h3 className="absolute left-6 top-20 md:top-90 lg:top-75 xl:top-97 font-medium">{title}</h3>
         </figure>
     </div>
   )
