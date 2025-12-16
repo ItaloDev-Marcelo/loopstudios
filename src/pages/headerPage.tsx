c16527b642c574e0873d7d28c264959afaa729c8
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-12-16 14:10:29 -0300

    refactor: add CommunProps to headerPage and pass props to navBar

--- src/pages/headerPage.tsx
+++ src/pages/headerPage.tsx
@@ -1,13 +1,16 @@
 import HeaderContainer from '../header-banner/HeaderContainer';
+import type { CommunProps } from '../interface/CommunProps';
 import NavBar from '../Nav/NavBar'
 
-const headerPage = () => {
+const headerPage = ({hundleNav,navTab}:CommunProps) => {
   return (
     <HeaderContainer>
-        <NavBar/>
+        <NavBar hundleNav={hundleNav} navTab={navTab} />
         <div className='border-2 border-white p-4 w-[325px] 
-        h-50 md:w-[400px] lg:w-[450px] m-auto  relative top-50 tb:top-10 md:top-20 lg:top-15 lg:mx-10 lg:my-0 xl:top-25'>
-            <h1 className='text-4xl lg:text-[3.3em] font-light text-gray-200 '>IMMERSIVE <br className='lg:hidden'/> EXPERIENCES <br className='lg:hidden'/>  THAT <br className='lg:hidden'/>  DELIVER</h1>
+        h-50 md:w-[400px] lg:w-[450px] m-auto  relative top-50 tb:top-10 md:top-20 
+        lg:top-15 lg:mx-10 lg:my-0 xl:top-25 lg:left-4'>
+            <h1 className='text-4xl lg:text-[3.3em] font-light text-gray-200 '>IMMERSIVE <br className='lg:hidden'/>
+             EXPERIENCES <br className='lg:hidden'/>  THAT <br className='lg:hidden'/>  DELIVER</h1>
         </div>
     </HeaderContainer>
   )
