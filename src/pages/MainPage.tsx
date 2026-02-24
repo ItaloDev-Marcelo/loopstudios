ff309fd30abfc122e5ecbe3fb9de15b9a8b044c7
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-02-24 14:00:15 -0300

    style: updated style and fix issues to tablet devices

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,12 +6,12 @@ const MainPage = () => {
   return (
     <main className='flex flex-col items-center justify-center md:items-start p-2'>
         <Section style='flex flex-col items-center lg:flex-row justify-center mt-10 
-        lg:mt-[-7em]  xl:mt-[5rem] lg:pl-17 '>
+        lg:mt-[-10em]  xl:mt-[5rem] lg:pl-17 '>
            <figure className='tb:p-7 md:p-9 lg:p-10'>
                <RenderImage mobile={Images[16]} desk={Images[17]} type={1} /> 
            </figure>
            <div className=' text-black text-center lg:text-left bg-white p-10
-            lg:w-[550px] lg:h-[355px] relative lg:top-27 lg:right-50'>
+            lg:w-137.5 lg:h-88.75 relative lg:top-27 lg:right-50 lg:mb-30 xl:my-0'>
                <h2 className='my-3 uppercase text-3xl '>The leader in interactive VR</h2>
                <p className='md:px-20 lg:px-0'> Founded in 2011, Loopstudios has been producing world-class virtual reality 
   projects for some of the best companies around the globe. Our award-winning 
@@ -21,7 +21,7 @@ const MainPage = () => {
         </Section>
 
         <Section style='flex flex-col  justify-center items-center xl:items-stretch
-         mt-10 lg:mt-25 xl:mt-25 lg:pr-5 xl:pl-30 mb-10'>
+         mt-10 lg:my-25 xl:mt-25 xl:mt-25 lg:pr-5 xl:pl-30 mb-10'>
            <div className=' flex fle-col xl:flex-row xl:justify-between text-center items-center py-2 mb-5'> 
                <h2 className='text-3xl xl:text-[3.5rem] flex '>Our Creations</h2> 
                <button className='hidden  xl:flex border-2 px-4 py-1.5 hover:bg-black hover:text-white'>SEE ALL</button>
--- src/pages/headerPage.tsx
+++ src/pages/headerPage.tsx
@@ -6,9 +6,9 @@ const headerPage = ({hundleNav,navTab}:CommunProps) => {
   return (
     <HeaderContainer>
         <NavBar hundleNav={hundleNav} navTab={navTab} />
-        <div className='border-2 border-white p-4 w-[325px] 
-        h-50 md:w-[400px] lg:w-[450px] m-auto  relative top-50 tb:top-10 md:top-20 
-        lg:top-15 lg:mx-10 lg:my-0 xl:top-25 lg:left-4'>
+        <div className='border-2 border-white p-4 w-81.25 
+        h-50 md:w-100 lg:w-112.5 m-auto  relative top-50 tb:top-10 md:top-20 
+        lg:top-12 lg:mx-10 lg:my-0 xl:top-25 lg:left-4'>
             <h1 className='text-4xl lg:text-[3.3em] font-light text-gray-200 '>IMMERSIVE <br className='lg:hidden'/>
              EXPERIENCES <br className='lg:hidden'/>  THAT <br className='lg:hidden'/>  DELIVER</h1>
         </div>
