0af68b22c1daaf128df6bd8be98268b5045a09d6
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-01 15:36:00 -0300

    style: fix layout hight and put all items to the center of the page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,9 +4,9 @@ import Card from '../components/Card'
 import RenderImage  from '../components/RenderImage'
 const MainPage = () => {
   return (
-    <main className='flex flex-col items-center justify-center md:items-start p-2'>
-        <Section style='flex flex-col items-center lg:flex-row justify-center 
-        lg:pl-17 '>
+    <main className='flex flex-col items-center justify-center md:items-start xl:items-center p-2 xl:gap-5'>
+        <Section style='flex flex-col  items-center lg:flex-row justify-center 
+        lg:pl-17 xl:my-5 '>
            <figure className='tb:p-7 md:p-9 lg:p-10'>
                <RenderImage mobile={Images[16]} desk={Images[17]} type={1} /> 
            </figure>
@@ -20,15 +20,15 @@ const MainPage = () => {
            </div>
         </Section>
 
-        <Section style='flex flex-col  justify-center items-center xl:items-stretch
-        lg:pr-5 xl:pl-30 mb-10'>
+        <Section style='flex flex-col xl:my-5   justify-center items-center xl:items-stretch
+        lg:pr-5 xl:pl-30 mb-10 relative xl:mt-20  place-items-center'>
            <div className=' flex fle-col xl:flex-row xl:justify-between text-center items-center py-2 mb-5'> 
                <h2 className='text-3xl xl:text-[3.5rem] flex '>Our Creations</h2> 
                <button className='hidden  xl:flex border-2 px-4 py-1.5 hover:bg-black hover:text-white'>SEE ALL</button>
            </div>
 
            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 md:p-3 xl:p-0
-           xl:grid-cols-4 gap-5 xl:gap-7 xl:mt-10 tb1:pl-13'>  
+           xl:grid-cols-4 gap-5 xl:gap-7 xl:mt-10 tb1:pl-13 bg-purple-200 p-30'>  
                {
                 Data.map((data) => (
                    <Card mobile={data.mobile} desk={data.desk} title={data.title} key={data.id} />
--- src/pages/headerPage.tsx
+++ src/pages/headerPage.tsx
@@ -8,8 +8,8 @@ const headerPage = ({hundleNav,navTab}:CommunProps) => {
         <NavBar hundleNav={hundleNav} navTab={navTab} />
         <div className='border-2 border-white  p-4 w-81.25 
         h-50  md:w-100 lg:w-112.5 m-auto  relative top-50 tb:top-10 md:top-5 
-        lg:top-12 lg:mx-10 lg:my-0 xl:top-25 lg:left-4'>
-            <h1 className='text-4xl lg:text-[3.3em] font-light text-gray-200 '>IMMERSIVE <br className='lg:hidden'/>
+        lg:top-12 lg:mx-10 lg:my-0 xl:top-35 lg:left-4 xl:left-20 xl:scale-125'>
+            <h1 className='text-4xl lg:text-[3.3em]  font-light text-gray-200 '>IMMERSIVE <br className='lg:hidden'/>
              EXPERIENCES <br className='lg:hidden'/>  THAT <br className='lg:hidden'/>  DELIVER</h1>
         </div>
     </HeaderContainer>
