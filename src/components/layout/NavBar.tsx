7d36a398fbb19b6050505a602167c9e17ece165d
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-03 12:06:17 -0300

    feat: add more style and importing images

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,8 +1,7 @@
-import NavItem from "../components/NavItem"
-import close from '../assets/images/icon-close.svg';
-import hamburger from '../assets/images/icon-hamburger.svg';
-import { navItems } from "../utils/Nav.items";
-import type { CommunProps } from "../interface/CommunProps";
+import NavItem from "../ui/NavItem"
+import { navItems } from "../../shared/constants/Nav.items";
+import type { CommunProps } from "../../shared/types/CommunProps";
+import { Close, Hamburger, Logo } from "../../utils/Image";
 
 export default function NavBar({hundleNav,navTab}:CommunProps) {
      
@@ -11,12 +10,12 @@ export default function NavBar({hundleNav,navTab}:CommunProps) {
          'bg-black h-screen lg:h-auto text-white lg:bg-transparent  '}
           lg:flex lg:items-center lg:justify-between lg:mt-2.5 p-7 lg:p-5 xl:p-11 `}>
              <div className='flex flex-row justify-between items-center mt-2.5 px-2 lg:mt-0 lg:ml-2.5'>
-                <a href='index.html' className='font-bold text-2xl text-white'>loopstudios</a>
-             <button id='btn' className='flex lg:hidden' onClick={hundleNav}>{navTab ? <img src={hamburger} className="is-closed"/> 
-              : <img src={close} className='is-open' />}</button>
+                <a href='index.html' className='w-45 xl:w-75 '><img src={Logo} alt='logo' /> </a>
+             <button id='btn' className='flex lg:hidden' onClick={hundleNav}>{navTab ? <img src={Hamburger} className="is-closed"/> 
+              : <img src={Close} className='is-open' />}</button>
              </div>
              <ul className={`${navTab ? 'hidden lg:flex lg:flex-row lg:items-center px-4 ' :
-                 ' mt-27 lg:mt-0 lg:flex lg:flex-row lg:items-center'} xl:px-5`}>
+                 ' mt-35 lg:mt-0 lg:flex lg:flex-row lg:items-center'} xl:px-5`}>
                 {navItems.map((item, index) => (
                     <NavItem navLink={item} key={index} />
                 ))}
