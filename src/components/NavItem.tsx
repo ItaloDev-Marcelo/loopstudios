f713e7d5ae9633bde328608c14e51c0864451ce3
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-03 11:54:39 -0300

    refactor: removing files from components folder

--- src/components/NavItem.tsx
+++
@@ -1,8 +0,0 @@
-import type { NavProps } from "../interface/NavProps"
-
-const NavItem = ({navLink}:NavProps) => {
-  return <li className='text-2xl  my-7 lg:my-0 lg:text-[1.1em] lg:mx-3.5
-   font-light uppercase lg:capitalize lg:font-semibold  text-gray-200 hover:text-white'><a href='#'>{navLink}</a></li>
-}
-
-export default NavItem
--- src/components/Section.tsx
+++
@@ -1,9 +0,0 @@
-import type { SectionProps } from "../interface/SectionProps"
-
-const Section = ({children,style}:SectionProps) => {
-  return (
-    <section className={style}>{children}</section>
-  )
-}
-
-export default Section
